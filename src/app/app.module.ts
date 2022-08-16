c0e8f9a06d27deba0ee3d0719e9abea35d341ca7
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-08-16 17:15:59 +0700

    up casestudy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
+import { CustomerComponent } from './customer-management/customer/customer.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FacilityComponent } from './facility/facility.component';
 import { ContractComponent } from './contract/contract.component';
-import { CustomerEditComponent } from './customer-edit/customer-edit.component';
-import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerEditComponent } from './customer-management/customer-edit/customer-edit.component';
+import { CustomerCreateComponent } from './customer-management/customer-create/customer-create.component';
 import { FacilityEditComponent } from './facility-edit/facility-edit.component';
 import { FacilityCreateComponent } from './facility-create/facility-create.component';
 import { ContractCreateComponent } from './contract-create/contract-create.component';
+import {RouterModule, Routes} from "@angular/router";
+import {CustomerRoutingModule} from "./customer-routing.module";
+import { CustomerManagementComponent } from './customer-management/customer-management.component';
+import {CommonModule} from "@angular/common";
+import { DeleteComponent } from './delete/delete.component';
+import {FacilityRoutingModule} from "./facility-routing.module";
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'customer'},
+  {path: 'customer', component: CustomerComponent},
+  {path: 'customer-create', component:CustomerCreateComponent},
+  {path: 'customer-edit', component:CustomerEditComponent},
+  {path: 'facility-create', component:FacilityCreateComponent},
+  {path: 'facility-edit', component:FacilityEditComponent},
+  {path: 'facility', component:FacilityComponent},
+  {path: 'contract', component:ContractComponent},
+  {path: 'contract-create', component:ContractCreateComponent}
+]
 
 @NgModule({
   declarations: [
@@ -25,12 +43,23 @@ import { ContractCreateComponent } from './contract-create/contract-create.compo
     CustomerCreateComponent,
     FacilityEditComponent,
     FacilityCreateComponent,
-    ContractCreateComponent
+    ContractCreateComponent,
+    CustomerManagementComponent,
+    HeaderComponent,
+    FooterComponent,
+    DeleteComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    CustomerRoutingModule,
+    FacilityRoutingModule,
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
